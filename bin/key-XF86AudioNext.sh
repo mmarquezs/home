0dfce92675280a791f2f844f7b6fbe87fc00f5a6
mmarquezs <mmsa1994@gmail.com>
2015-10-22 21:18:02 +0200

    Added some bin files

---
+++ bin/key-XF86AudioNext.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+process_running() {
+    [[ `pgrep -f $1` ]] && return 0 || return 1
+}
+
+#tune play $(tune list | grep "`tune show | rev | cut -d '(' -f 2- | rev | sed -e 's/[[:space:]]*$//'`" | cut -d '$' -f 2 | cut -d '[' -f 2 | cut -d ']' -f 1)
+
+if process_running 'radiotray';
+then
+    if [[ `tune show | grep '(not playing)'` ]]
+    then
+	tune play $(($(tune list | grep "`tune show | rev | cut -d '(' -f 2- | rev | sed -e 's/[[:space:]]*$//'`" | cut -d '$' -f 2 | cut -d '[' -f 2 | cut -d ']' -f 1)+1)) > /dev/null
+    else
+	tune stop > /dev/null
+	tune play $(($(tune list | grep "`tune show | rev | cut -d '(' -f 2- | rev | sed -e 's/[[:space:]]*$//'`" | cut -d '$' -f 2 | cut -d '[' -f 2 | cut -d ']' -f 1)+1)) > /dev/null
+
+    fi
+    
+fi
+   
