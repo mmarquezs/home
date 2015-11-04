97ff1c13ed6c222f0d5dd9360a80ed4f461e1e89
mmarquezs <mmsa1994@gmail.com>
2015-11-04 21:53:06 +0100

    Some changes in emacs packages

---
+++ .emacs.d/elpa/dom-20150414.1610/dom-autoloads.el
@@ -0,0 +1,15 @@
+;;; dom-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dom.el") (22071 40588 526924 553000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dom-autoloads.el ends here
