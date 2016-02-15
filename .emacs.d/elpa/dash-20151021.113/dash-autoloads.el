c17543a6b56081ed455974bfb4ee2847a6b56223
mmarquezs <mmsa1994@gmail.com>
2016-02-15 14:22:15 +0100

    Removing unecessary file in .emacs.d

--- .emacs.d/elpa/dash-20151021.113/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22067 18663 707166 181000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
